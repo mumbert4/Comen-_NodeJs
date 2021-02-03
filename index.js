205b523d380dff9769a9d805812155fd89636607
Miquel Umbert <miquel.umbert.bosch@gmail.com>
2021-02-04 00:42:50 +0100

    fs (file sistem)

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const os = require('os');
-//const server= require('server');
+const fs = require('fs');//file sistem
 
-console.log(os.platform());
-console.log(os.release());
-console.log('free mem: ',os.freemem(), ' bytes de memoria lliure');//retorna memeoria lliure en bytes
-console.log('Total mem: ',os.totalmem(), ' bytes de memoria total');
-console.log('Memoria usada: ', os.totalmem() - os.freemem(), 'bytes de memoria usats');
+// fs.writeFile('./texte.txt','linia u',function (err){// cream un archiu texte.txt dins el mateix directori amb el texte "linia u"
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('Archiu creat');
+// });
 
+// console.log('darrera linia de codi');
+
+fs.readFile('./texte.txt', function(err,data){
+    if(err){
+        console.log(err);
+    }
+    console.log(data.toString());//txte.toString()-> passa les dades a string per a poder entender-ho sinos surt un buffer de simbols que no entenem
+});
