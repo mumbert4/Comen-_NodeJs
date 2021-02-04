b329ee1229bb11568f1215260699c6cf86d24efc
Miquel Umbert <miquel.umbert.bosch@gmail.com>
2021-02-04 01:36:36 +0100

    Express

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-const http = require('http');
+// const http = require('http');
+
+// const colors = require('colors');
+// // console.log(http);
+
+// // http.createServer(function(req, res){//req -> request, res -> respond
+// //     res.writeHead(404, {'content-type': 'text/plain'});// aixo son codis per saber si la peticio ha anat be o no i si no hi anat a on ha fallat
+// //     res.write('Supa Hot fire');
+// //     res.end();//acabar la resposta
+// // }).listen(3000);
+
+// //realitza la mateixa funcio
+
+// const handleServer = function(req, res){//req -> request, res -> respond
+//     res.writeHead(200, {'content-type': 'text/html'});// aixo son codis per saber si la peticio ha anat be o no i si no hi anat a on ha fallat
+//     res.write('<h1>Supa Hot fire</h1>');
+//     res.end();//acabar la resposta
+// }
+
+// const server= http.createServer(handleServer);
+
+// server.listen(3000,function(){//feim escoltar el servidor pel port 3000
+//     console.log('Server on port 3000'.yellow);
+// });
+
+
+// //npmjs.com repositori de codi de js de molta gent 
+
 
 const colors = require('colors');
-// console.log(http);
 
-// http.createServer(function(req, res){//req -> request, res -> respond
-//     res.writeHead(404, {'content-type': 'text/plain'});// aixo son codis per saber si la peticio ha anat be o no i si no hi anat a on ha fallat
-//     res.write('Supa Hot fire');
-//     res.end();//acabar la resposta
-// }).listen(3000);
+const express = require('express');
 
-//realitza la mateixa funcio
+const server = express();
 
-const handleServer = function(req, res){//req -> request, res -> respond
-    res.writeHead(200, {'content-type': 'text/html'});// aixo son codis per saber si la peticio ha anat be o no i si no hi anat a on ha fallat
-    res.write('<h1>Supa Hot fire</h1>');
-    res.end();//acabar la resposta
-}
+server.get('/',function (req,res){
+    res.send('<h1>Treballant amb Express i NodeJs</h1>')
+})
 
-const server= http.createServer(handleServer);
-
-server.listen(3000,function(){//feim escoltar el servidor pel port 3000
-    console.log('Server on port 3000'.yellow);
-});
-
-
-//npmjs.com repositori de codi de js de molta gent 
+server.listen(3000,function (){
+    console.log('Server on port 3000'.red)
+})
